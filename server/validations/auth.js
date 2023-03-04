8e48cc8784a0f210eeb991f660b1210bff085657
irakli <i.mikava365@gmail.com>
2023-03-04 09:22:24 +0400

    qq

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 export const registerValidator = [
   body("email").isEmail(),
-  body("password", "irefek jfiro ii").isLength({ min: 5 }),
+  body("password").isLength({ min: 5 }),
   body("fullName").isLength({ min: 3 }),
   body("avatarURL").optional().isURL(),
 ];
